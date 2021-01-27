29cc0090ca60f34069072894917e0b854eba911e
Hadi Maher <hady207@hotmail.com>
2021-01-27 20:42:23 +0300

    socket io setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import socket from 'socket.io';
 import loaders from './loaders';
 // config() will read your .env file, parse the contents, assign it to process.env.
 dotenv.config();
@@ -8,12 +9,19 @@ async function startServer() {
   const app = express();
   await loaders(app);
 
-  app.listen(process.env.PORT, (err) => {
+  const server = app.listen(process.env.PORT, (err) => {
     if (err) {
       console.log(err);
       return;
     }
     console.log(`Your server is running on port ${process.env.PORT} !`);
+  });
+
+  // Socket setup
+  const io = socket(server);
+
+  io.on('connection', (socket) => {
+    console.log('connect');
   });
 }
 
